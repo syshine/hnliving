6db47e0abe4061407c7087fb83db0117c5a75513
syshine <shishuai0805@163.com>
2018-11-29 15:48:25 +0800

    canvas

--- hnliving.web/App_Start/BundleConfig.cs
+++ hnliving.web/App_Start/BundleConfig.cs
@@ -37,7 +37,8 @@ namespace hnliving.web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/color.css"));
         }
     }
 }
