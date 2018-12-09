45112a9b569d8cf5202b8eb8febe91cc137458c5
syshine <43083537+syshine@users.noreply.github.com>
2018-12-09 22:43:46 +0800

    Anime.js

--- hnliving.web/Areas/Study/Controllers/Program/AnimeJsController.cs
+++ hnliving.web/Areas/Study/Controllers/Program/AnimeJsController.cs
@@ -14,12 +14,65 @@ namespace hnliving.web.Areas.Study.Controllers.Program
             return View();
         }
 
+        /// <summary>
+        /// 动画目标
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Targets()
         {
             return View();
         }
 
+        /// <summary>
+        /// 属性目标
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Attribute()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 动画参数
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Parameter()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 函数返回动画参数
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult FuncRetPar()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 方向和循环
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DirAndLoop()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 动画赋值
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Assign()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 时间轴
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TimeLine()
         {
             return View();
         }
--- hnliving.web/Navigate/AnimeJsNav.js
+++ hnliving.web/Navigate/AnimeJsNav.js
@@ -6,6 +6,21 @@
     }, {
         "title": "属性变化",
         "url": "/study/program/AnimeJs/Attribute"
+    }, {
+        "title": "动画参数",
+        "url": "/study/program/AnimeJs/Parameter"
+    }, {
+        "title": "函数返回动画参数",
+        "url": "/study/program/AnimeJs/FuncRetPar"
+    }, {
+        "title": "方向和循环",
+        "url": "/study/program/AnimeJs/DirAndLoop"
+    }, {
+        "title": "动画赋值",
+        "url": "/study/program/AnimeJs/Assign"
+    }, {
+        "title": "时间轴",
+        "url": "/study/program/AnimeJs/TimeLine"
     }
     ]
 }
