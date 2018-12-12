9f220a1fcebee0cf370e6086713041344f88afe8
syshine <43083537+syshine@users.noreply.github.com>
2018-12-12 21:31:38 +0800

    Anime.js

--- hnliving.web/Navigate/AnimeJsNav.js
+++ hnliving.web/Navigate/AnimeJsNav.js
@@ -1,6 +1,9 @@
 ﻿var navAj = {
     "title": "Bootstrap",
     "navList": [{
+        "title": "Anime.js首页",
+        "url": "/study/program/AnimeJs/Index"
+    }, {
         "title": "动画目标",
         "url": "/study/program/AnimeJs/Targets"
     }, {
