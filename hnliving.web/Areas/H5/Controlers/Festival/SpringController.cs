4e0ed5c667142e40c8c92e0dd6ea255c9953f182
syshine <shishuai0805@163.com>
2018-12-24 18:14:17 +0800

    H5

---
+++ hnliving.web/Areas/H5/Controlers/Festival/SpringController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hnliving.web.Areas.H5.Controlers.Festival
+{
+    public class SpringController : Controller
+    {
+        // GET: H5/Festival/Spring
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Y2019()
+        {
+            return View();
+        }
+    }
+}
---
+++ hnliving.web/Areas/H5/H5AreaRegistration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hnliving.web.Areas.H5
+{
+    public class H5AreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "H5";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+
+            context.MapRoute(
+                name: "H5_Festival",
+                url: "H5/Festival/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "hnliving.web.Areas.H5.Controllers.Festival" }
+            );
+            context.MapRoute(
+                name: "H5_Business",
+                url: "H5/Business/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "hnliving.web.Areas.H5.Controllers.Business" }
+            );
+
+            context.MapRoute(
+                name: "H5_Demo",
+                url: "H5/Demo/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "hnliving.web.Areas.H5.Controllers.Demo" }
+            );
+
+            //此路由不能删除
+            context.MapRoute(name: "H5_default",
+                             url: "H5/{controller}/{action}/{id}",
+                             defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                             namespaces: new[] { "hnliving.web.Areas.H5.Controllers" });
+
+        }
+    }
+}
