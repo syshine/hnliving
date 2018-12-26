0fb6eea73049433239f85a8e19631b1950cf6ac6
syshine <shishuai0805@163.com>
2018-12-26 15:33:04 +0800

    H5

---
+++ hnliving.web/Areas/H5/Controllers/Demo/SvgController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hnliving.web.Areas.H5.Controllers.Demo
+{
+    public class SvgController : Controller
+    {
+        // GET: H5/Demo/Svg
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: H5/Demo/Svg
+        public ActionResult Test1()
+        {
+            return View();
+        }
+    }
+}
